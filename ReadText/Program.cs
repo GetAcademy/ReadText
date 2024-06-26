458c0312a337acdefdd968177016aec089d9f70c
GET Academy <terje@kolderup.net>
2024-06-26 13:22:11 +0200

    Initial commit

---
+++ ReadText/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReadText;
+using TextReader = ReadText.TextReader;
+
+Console.WriteLine("Hello, World!");
+
+var textReader = new TextReader("Velg kategori", "forrett", "middag", "dessert");
+var category = textReader.ReadText();
+
+Console.WriteLine($"Du valgte {category}.");
---
+++ ReadText/TextReader.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace ReadText
+{
+    internal class TextReader
+    {
+        private string[] _options;
+        private StringBuilder _text;
+
+        public TextReader(string question, params string[] options)
+        {
+            _text = new StringBuilder();
+            _options = options;
+            Console.WriteLine(question);
+        }
+
+        public string ReadText()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        return _text.ToString();
+                    }
+
+                    if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        var option = _options.FirstOrDefault(o => o.StartsWith(_text.ToString()));
+                        if (option != null)
+                        {
+                            Console.CursorTop++;
+                            var foreColor = Console.ForegroundColor;
+                            var backColor = Console.BackgroundColor;
+                            Console.ForegroundColor = backColor;
+                            Console.BackgroundColor = foreColor;
+                            Console.WriteLine(option);
+                            Console.ForegroundColor = foreColor;
+                            Console.BackgroundColor = backColor;
+                            Console.CursorTop--;
+                        }
+                    }
+                    else
+                    {
+                        _text.Append(key.KeyChar);
+                        Console.CursorLeft = 0;
+                        Console.Write(_text.ToString());
+                        var option = _options.FirstOrDefault(o => o.StartsWith(_text.ToString()));
+                        if (option != null)
+                        {
+                            Console.CursorTop++;
+                            Console.WriteLine(option);
+                            Console.CursorTop--;
+                        }
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(200);
+                }
+            }
+
+            return null;
+        }
+    }
+}
